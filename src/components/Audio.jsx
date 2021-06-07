af3c29ee126fbbc0092b6319c4384d6a7213ba82
Urmi-Jana <75936174+Urmi-Jana@users.noreply.github.com>
2021-06-08 00:27:06 +0530

    Added the Audio Component

---
+++ src/components/Audio.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Recorder from "./Recorder";
+
+function Audio(){
+
+
+    let [audioURL, isRecording, startRecording, stopRecording] = Recorder();
+    return(
+    <div className = "audio">
+        <audio className = "player" src = {audioURL} controls />
+
+        <button
+         className = "click"
+         onClick = {startRecording} 
+         disabled = {isRecording} 
+          >Start</button>
+
+        <button 
+        className = "click" 
+        onClick = {stopRecording} 
+        disabled = {!isRecording} 
+         >Stop</button>
+    </div>
+    )
+}   
+
+export default Audio;
+
+
